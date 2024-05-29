1c7adee12a96f08eb6de956a982fe9a2dc8b8ca8
Claire Vannier <clairevannierpro@gmail.com>
2024-05-29 16:05:07 +0200

    modified string in french

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         Lion lion = new Lion("Lion", 120, 4, true);
-        Parrot parrot = new Parrot("Parrot", 20, 2, true);
+        Parrot parrot = new Parrot("Pérroquet", 20, 2, true);
 
         System.out.println(lion.introduce());
-        System.out.println("Is lion dangerous? " + lion.isDangerous());
+        System.out.println("Le lion est-il dangereux ? " + lion.isDangerous());
         lion.hunt();
 
         System.out.println(parrot.introduce());
-        System.out.println("Is parrot dangerous? " + parrot.isDangerous());
+        System.out.println("Le perroquet est-il dangereux ? " + parrot.isDangerous());
         parrot.fly();
     }
 }
